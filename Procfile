91622ca671c086302c0e86c2868237f3618786bc
bruno okamoto <brunookamoto11@gmail.com>
2020-07-22 16:14:14 -0300

    deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:gunicorn commerce.wsgi --log-file -
+web: gunicorn commerce.wsgi --log-file -
--- commerce/settings.py
+++ commerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
 
 STATICFIELS_DIRS = [
     os.path.join(BASE_DIR, 'auctions/static')
